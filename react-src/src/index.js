9b65868daff6afe927b16717fda0feb429f6dc79
Anddreas Willibald Weber <andreas.willibald.weber@gmail.com>
2023-08-28 00:27:30 +0200

    redo createRoot to fix build

--- react-src/src/index.js
+++ react-src/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { createRoot } from "react-dom/client"
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { init } from "@neutralinojs/lib"
 
-init();
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
 );
+
+init();
