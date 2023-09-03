febcf01440973bf6ec990aec7f34c5b0b48691f0
Anddreas Willibald Weber <andreas.willibald.weber@gmail.com>
2023-09-03 16:41:41 +0200

    fix indentation

--- proggame-react/src/components/AppOverlay.js
+++ proggame-react/src/components/AppOverlay.js
@@ -21,25 +21,24 @@ export default function AppOverlay({state, setState, setOut, tasks, setTasks, in
     return (state === -3 || state === -1 || state === 5) ? (
         <Overlay blur={15} center>
             <Stack>
-                
-                    {state === -1 ?  <Card>
-                                        <Title color="gray.6">Challenge Rules</Title>
-                                        <Space h="xl"/>
-                                        <List spacing="xs"
-                                            size="sm"
-                                            center
-                                            icon={
-                                                <ThemeIcon color="red.5" size={28} radius="xl">
-                                                    <AlertCircle size="20" />
-                                                </ThemeIcon>
-                                            }
-                                        >
-                                            <List.Item><Text fz="xl" color="gray.6">Do not use print()!</Text></List.Item>
-                                            <List.Item><Text fz="xl" color="gray.6">The code must be testable!</Text></List.Item>
-                                            <List.Item><Text fz="xl" color="gray.6">Code must work the first time it is executed!</Text></List.Item>
-                                        </List>
-                                        <Space h="xl"/>
-                                    </Card>: <></>}
+                {state === -1 ?  <Card>
+                                    <Title color="gray.6">Challenge Rules</Title>
+                                    <Space h="xl"/>
+                                    <List spacing="xs"
+                                        size="sm"
+                                        center
+                                        icon={
+                                            <ThemeIcon color="red.5" size={28} radius="xl">
+                                                <AlertCircle size="20" />
+                                            </ThemeIcon>
+                                        }
+                                    >
+                                        <List.Item><Text fz="xl" color="gray.6">Do not use print()!</Text></List.Item>
+                                        <List.Item><Text fz="xl" color="gray.6">The code must be testable!</Text></List.Item>
+                                        <List.Item><Text fz="xl" color="gray.6">Code must work the first time it is executed!</Text></List.Item>
+                                    </List>
+                                    <Space h="xl"/>
+                                </Card>: <></>}
                 <Button size="xl" color={state === 5 ? 'green.5' : state === -1 ? 'orange.5' : 'red.5'}
                         radius="xl" 
                         onClick={() => state === -3 || state === 5 ? (setState(-2), setName(""), clear()) : (shuffle(), setState(0), clear())}
