e3689cd9631862c12cd00bb753306b4bdb3df624
Anddreas Willibald Weber <andreas.willibald.weber@gmail.com>
2023-09-03 12:55:21 +0200

    fix overlay message color and size

--- proggame-react/src/components/AppOverlay.js
+++ proggame-react/src/components/AppOverlay.js
@@ -13,7 +13,7 @@ export default function AppOverlay({state, setState, setOut, initVal, setInitVal
 
     return (state === -3 || state === -1 || state === 5) ? (
         <Overlay blur={15} center>
-            <Button color={state === 5 ? 'green.5' : 'red.5'}
+            <Button size="xl" color={state === 5 ? 'green.5' : state === -1 ? 'orange.5' : 'red.5'}
                     radius="xl" 
                     onClick={() => state === -3 || state === 5 ? (setState(-2), setName(""), clear()) : (setState(0), clear())}>
                 {state === 5 ? congratulations : state === -3 ? failured : challange}
